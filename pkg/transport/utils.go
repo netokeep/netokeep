32a804441131db07468212e68e12f6d61d5a7f25
MUGE TONG <here5320@gmail.com>
2026-03-11 13:22:08 +0800

    update Relay

--- pkg/transport/utils.go
+++ pkg/transport/utils.go
@@ -6,27 +6,27 @@ import (
 	"sync"
 )
 
-// Relay 在两个 net.Conn 之间进行双向数据转发
-func Relay(left, right net.Conn) {
+/*
+Relay helps to forward data between two net.Conn connections in both directions.
+*/
+func Relay(left net.Conn, right net.Conn) {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	// 1. 从左边读，往右边写
+	// Read the left connection and write to the right connection
 	go func() {
 		defer wg.Done()
 		_, _ = io.Copy(right, left)
-		// 一旦这边断了，通知另一边也别读了
 		right.Close()
 	}()
 
-	// 2. 从右边读，往左边写
+	// Read the right connection and write to the left connection
 	go func() {
 		defer wg.Done()
 		_, _ = io.Copy(left, right)
-		// 一旦这边断了，通知另一边也别读了
 		left.Close()
 	}()
 
-	// 等待两个方向都结束
+	// Wait for both directions to finish
 	wg.Wait()
 }
