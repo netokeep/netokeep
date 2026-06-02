e6c7cba56ecc56449b191d050a9cd4f9d7937d7b
MUGE_TONG <here5320@gmail.com>
2026-06-02 12:48:55 +0000

    update nk stop logic. pid file and port file will not be deleted

--- cmd/nk/stop.go
+++ cmd/nk/stop.go
@@ -45,13 +45,13 @@ func createStopCmd() *cobra.Command {
 			}
 
 			if success {
-				// Cleanup the PID file after successful stop
-				if err := local.RemovePID(name); err != nil {
-					fmt.Printf("Warning: Failed to remove PID file: %v\n", err)
-				}
-				if err := local.RemovePort(name + "ssh"); err != nil {
-					fmt.Printf("Warning: Failed to remove Port file: %v\n", err)
-				}
+				// // Cleanup the PID file after successful stop
+				// if err := local.RemovePID(name); err != nil {
+				// 	fmt.Printf("Warning: Failed to remove PID file: %v\n", err)
+				// }
+				// if err := local.RemovePort(name + "ssh"); err != nil {
+				// 	fmt.Printf("Warning: Failed to remove Port file: %v\n", err)
+				// }
 				fmt.Println("\033[32mstopped\033[0m")
 			} else {
 				fmt.Println("\nWarning: Process is taking too long to stop. You might need to kill it manually.")
